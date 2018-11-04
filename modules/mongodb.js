f654091277f2e172d056ee5e174f9895365bf8ab
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-04 14:16:15 +0000

    Added functionality for mongodb calls

--- modules/mongodb.js
+++ modules/mongodb.js
@@ -1,27 +1,17 @@
+// Import package for mongodb client
 const MongoClient = require('mongodb').MongoClient
 
-// const url = "mongodb://localhost:27017/articles_store";
-
-// const articles_collection = "articles"
-
+// Function for creating the initial database
+// Requires the url and name for the new database
 exports.createDatabase = function(database_url, database_name) {
 
-    // Create table/collection
+    // Connecting to the url creates the database if it doesn't exist
+    // If this is run after creation it will not overwrite the existing mongodb database
     MongoClient.connect(database_url, function(err, db) {
 
         if (err) throw err
         console.log(database_name + " mongodb database has been created at url: " + database_url)
         db.close()
-
-        // Create new database object
-        const new_mongo_db = db.db(database_name);
-
-        // Create new mongodb collection called articles
-        new_mongo_db.createCollection(articles_collection, function(err, res) {
-            if (err) throw err;
-            console.log("Collection created!");
-            db.close();
-        });
     });
 }
 
@@ -30,9 +20,9 @@ exports.createCollection = function(database_url, database_name, collection_name
     MongoClient.connect(database_url, function(err, db) {
         if (err) throw err;
 
-        const dbo = db.db(database_name);
+        const new_mongo_db = db.db(database_name);
 
-        dbo.createCollection(collection_name, function(err, res) {
+        new_mongo_db.createCollection(collection_name, function(err, res) {
           if (err) throw err;
           console.log("Collection " + collection_name + " has been created in database: " + database_name);
           db.close();
@@ -74,7 +64,7 @@ exports.getAllFromCollection = function(database_url, database_name, collection_
     });
 }
 
-exports.getArticle = function(database_url, database_name, collection_name, query_object) {
+exports.getResourceFromCollection = function(database_url, database_name, collection_name, query_object) {
 
     MongoClient.connect(database_url, function(err, db) {
 
@@ -90,10 +80,33 @@ exports.getArticle = function(database_url, database_name, collection_name, quer
 
 }
 
-exports.updateArticle = function(article) {
+exports.updateResource = function(database_url, database_name, collection_name, query_object, new_values_object) {
+
+    MongoClient.connect(database_url, function(err, db) {
+
+        if (err) throw err
+        let dbo = db.db(database_name)
+
+        dbo.collection(collection_name).updateOne(query_object, new_values_object, function(err, res) {
+          if (err) throw err;
+          console.log("Resource updated");
+          db.close();
+        });
+    });
 
 }
 
-exports.deleteArticle = function(article) {
+exports.deleteArticle = function(database_url, database_name, collection_name, query_object) {
 
+    MongoClient.connect(database_url, function(err, db) {
+
+        if (err) throw err;
+        let dbo = db.db(database_name);
+
+        dbo.collection(collection_name).deleteOne(query_object, function(err, obj) {
+          if (err) throw err;
+          console.log("Resource deleted");
+          db.close();
+        });
+      });
 }
