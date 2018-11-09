bf59ddfd924a33689902e50c99b7d5bfb2a139bf
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-09 22:09:53 +0000

    Changed endpoints to use async/await functions

--- api-server.js
+++ api-server.js
@@ -1,3 +1,5 @@
+'use strict'
+
 console.log("Server Booting Up...");
 
 // Using express as my web server, create instance and set attributes
@@ -8,144 +10,146 @@ app.use(express.json());
 // Port this server will run on
 const port = 8080;
 
-const database_controller = require('./modules/database-controller')
-const favourite_recipes_controller = require('./modules/favourite-recipes-controller')
-const images_controller = require('./modules/images-controller')
-const logins_controller = require('./modules/logins-controller')
-const notifications_controller = require('./modules/notifications-controller')
-const ratings_controller = require('./modules/ratings-controller')
-const recipe_images_controller = require('./modules/recipe-images-controller')
-const recipes_controller = require('./modules/recipes-controller')
-const users_controller = require('./modules/users-controller')
-
-// Create a new database
-app.get('/createDatabase', (req, res) => {
-
-	database_controller.create()
-	console.log('Articles table created')
-})
+const databaseController = require('./modules/database-controller')
+const favourite_recipesController = require('./modules/favourite-recipes-controller')
+const imagesController = require('./modules/images-controller')
+const loginsController = require('./modules/logins-controller')
+const notificationsController = require('./modules/notifications-controller')
+const ratingsController = require('./modules/ratings-controller')
+const recipeImagesController = require('./modules/recipe-images-controller')
+const recipesController = require('./modules/recipes-controller')
+const usersController = require('./modules/users-controller')
 
 // Home root currently redirects to /recipes
-app.get('/', (req, res) => {
-	res.redirect('/recipes')
+app.get('/api/v1.0/', (req, res) => {
+	res.redirect('/api/v1.0/recipes')
 })
 
 // GET Request to retrieve all recipes
-app.get('/recipes', (req, res) => {
+app.get('/api/v1.0/recipes', async(req, res) => {
 
 	// Call controller to retrieve all recipes
-	// Once completed, callback function sends the result as a json string
-	recipes_controller.getAll(null, (recipes) => {
-		res.json(recipes)
-	})
+	// Waits for response from controller before continuing (async/await)
+	const recipes = await recipesController.getAll()
+
+	res.status(200).send(recipes)
 })
 
 // GET Request to retrieve one recipe
-app.get('/recipes/:recipe_id', (req, res) => {
+app.get('/api/v1.0/recipes/:recipe_id', async(req, res) => {
 
 	// Call controller to retrieve one recipe
-	// Once completed, callback function sends the result as a json string
-	recipes_controller.getById(req.params.recipe_id, (recipe) => {
-		res.json(recipe)
-	})
+	const recipe = await recipesController.getById(req.params.recipe_id)
+
+	res.status(200).json(recipe)
 })
 
 // POST Request to create a new recipe
-app.post('/recipes', (req, res) => {
+app.post('/api/v1.0/recipes', async(req, res) => {
 
 	// Call controller to create a new recipe from the provided request
-	// Once completed, run the callback which sends the client a message and status code confirming the recipe was created
-	recipes_controller.add(req.body, () => {
-		res.status(200).send("New recipe created\n")
-	})
+	const response = await recipesController.add(req.body)
+	
+	if(response) {
+		res.status(200).send("Recipe added succesfully\n")
+	} else {
+		res.status(400).send("There was an error posting your recipe\n")
+	}
 })
 
 // PUT Request to update a recipe
-app.put('/recipes/:recipe_id', (req, res) => {
+app.put('/api/v1.0/recipes/:recipe_id', async(req, res) => {
 
 	// Call controller to create a new recipe from the provided request
-	// Once completed, run the callback which sends the client a message and status code confirming the recipe was created
-	recipes_controller.update(req.params.recipe_id, req.body, () => {
-		res.status(200).send("Recipe with id: " + req.params.recipe_id + " has been updated\n")
-	})
+	const recipeUpdateResponse = await recipesController.update(req.params.recipe_id, req.body)
+
+	if(recipeUpdateResponse) {
+		res.status(200).send("recipe with id: " + req.params.recipe_id + " has been updated\n")
+	} else {
+		res.status(400).send("There was an error updating your recipe\n")
+	}	
 })
 
 // DELETE Request to delete one recipe
-app.delete('/recipes/:recipe_id', (req, res) => {
+app.delete('/api/v1.0/recipes/:recipe_id', async(req, res) => {
 
 	// Call controller to delete a recipe corresponding to the HTML request's recipe id
 	// Once completed, return back to client a message and status code confirming the recipe was deleted
-	recipes_controller.delete(req.params.recipe_id, req.body, () => {		
+	const recipeDeleteResponse = await recipesController.delete(req.params.recipe_id)
+
+	if(recipeDeleteResponse) {
 		res.status(200).send("Recipe with id: " + req.params.recipe_id + " has been deleted\n")
-	})
+	} else {
+		res.status(400).send("There was an error deleting your recipe\n")
+	}
 })
 
-app.get('/users', (req, res) => {
-
-
-})
-
-app.post('/users', (req, res) => {
+app.get('/api/v1.0/users', (req, res) => {
 
 
 })
 
-app.get('/ratings', (req, res) => {
+app.post('/api/v1.0/users', (req, res) => {
 
 
 })
 
-app.post('/ratings', (req, res) => {
+app.get('/api/v1.0/ratings', (req, res) => {
 
 
 })
 
-app.get('/images', (req,res) => {
+app.post('/api/v1.0/ratings', (req, res) => {
 
 
 })
 
-app.post('/images', (req,res) => {
+app.get('/api/v1.0/images', (req,res) => {
 
 
 })
 
-app.get('/notifications', (req, res) => {
+app.post('/api/v1.0/images', (req,res) => {
 
 
 })
 
-app.post('/notifications', (req,res) => {
+app.get('/api/v1.0/notifications', (req, res) => {
 
 
 })
 
-app.get('/favourite_recipes', (req, res) => {
+app.post('/api/v1.0/notifications', (req,res) => {
 
 
 })
 
-app.post('/favourite_recipes', (req, res) => {
+app.get('/api/v1.0/favourite_recipes', (req, res) => {
 
 
 })
 
-app.get('/recipe_images', (req, res) => {
+app.post('/api/v1.0/favourite_recipes', (req, res) => {
 
 
 })
 
-app.post('/recipe_images', (req, res) => {
+app.get('/api/v1.0/recipe_images', (req, res) => {
 
 
 })
 
-app.get('/logins', (req, res) => {
+app.post('/api/v1.0/recipe_images', (req, res) => {
 
 
 })
 
-app.post('/logins', (req, res) => {
+app.get('/api/v1.0/logins', (req, res) => {
+
+
+})
+
+app.post('/api/v1.0/logins', (req, res) => {
 
 
 })
