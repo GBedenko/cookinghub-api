06fa2da63b5aaf12aa1222ac90d9888cbacb5d6d
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-11 14:22:47 +0000

    Implemented controller logic for users

--- modules/users-controller.js
+++ modules/users-controller.js
@@ -1,22 +1,66 @@
 const database_url = "mongodb://localhost:27017/yummy_recipes"
-const users_collection_name = "users"
+const users_collection = "users"
 
-exports.add = function(usersObject){
+const database = require('./mongodb')
 
-};
+// Function to add a new user
+exports.add = async(userObject) => {
 
-exports.getById = function(conData, req, callback){
+    let addUser = database.addResourceToCollection(database_url, users_collection, userObject)
+                        .then((result) => result)
+                        .catch((err) => console.log(err))
 
-};
+    let addUserResponse = await addUser
 
-exports.getAll = function(conData, req, callback){
+    return addUserResponse
+}
 
-};
+// Function to retrieve one user
+exports.getById = async(userId) => {
 
-exports.update = function(usersObject){
+    let getUser = database.getResourceFromCollection(database_url, users_collection, userId)
+                        .then((user) => user)
+                        .catch((err) => console.log(err))
+    
+    let user = await getUser
 
-};
+    return user
+}
 
-exports.delete = function(usersObject){
+// Function to retrieve all users
+exports.getAll = async() => {
 
-};
+    let results = database.getAllFromCollection(database_url, users_collection)
+                    .then((results) => results) // Obtains the result from the Promise object
+                    .catch((err) => console.log(err)) // If the result was an error then handle the error
+    
+    // Calls the results function, waits for response before continuing
+    let final_result = await results
+
+    // Return the list of users
+    return final_result
+}
+
+// Function to update a user
+exports.update = async(userID, newUserDetailsObject) => {
+    
+    let updateUser = database.updateResource(database_url, users_collection, userID, newUserDetailsObject)
+                            .then((user) => user)
+                            .catch((err) => console.log(err))
+
+    let updateUserResponse = await updateUser
+
+    return updateUserResponse
+}
+
+// Function to delete a user
+exports.delete = async(userID) => {
+
+    let deleteUser = database.deleteResource(database_url, users_collection, userID)
+                            .then((user) => user)
+                            .catch((err) => console.log(err))
+
+    let deleteUserResponse = await deleteUser
+
+    return deleteUserResponse
+}
