1ad9e6e1605243482d27ae20c2fe994de2972b51
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-11 14:27:35 +0000

    Controller linking to DB for Ratings

--- modules/ratings-controller.js
+++ modules/ratings-controller.js
@@ -1,22 +1,66 @@
 const database_url = "mongodb://localhost:27017/yummy_recipes"
-const ratings_collection_name = "ratings"
+const ratings_collection = "ratings"
 
-exports.add = function(ratingsObject){
+const database = require('./mongodb')
 
-};
+// Function to add a new rating
+exports.add = async(ratingObject) => {
+    
+    let addRating = database.addResourceToCollection(database_url, ratings_collection, ratingObject)
+                        .then((result) => result)
+                        .catch((err) => console.log(err))
 
-exports.getById = function(conData, req, callback){
+    let addRatingResponse = await addRating
 
-};
+    return addRatingResponse
+}
 
-exports.getAll = function(conData, req, callback){
+// Function to retrieve one rating
+exports.getById = async(ratingId) => {
+    
+    let getRating = database.getResourceFromCollection(database_url, ratings_collection, ratingId)
+                        .then((rating) => rating)
+                        .catch((err) => console.log(err))
+    
+    let rating = await getRating
 
-};
+    return rating
+}
 
-exports.update = function(ratingsObject){
+// Function to retrieve all ratings
+exports.getAll = async() => {
+    
+    let results = database.getAllFromCollection(database_url, ratings_collection)
+                    .then((results) => results) // Obtains the result from the Promise object
+                    .catch((err) => console.log(err)) // If the result was an error then handle the error
+    
+    // Calls the results function, waits for response before continuing
+    let final_result = await results
 
-};
+    // Return the list of ratings
+    return final_result
+}
 
-exports.delete = function(ratingsObject){
+// Function to update a rating
+exports.update = async(ratingID, newRatingDetailsObject) => {
 
-};
+    let updateRating = database.updateResource(database_url, ratings_collection, ratingID, newRatingDetailsObject)
+                            .then((rating) => rating)
+                            .catch((err) => console.log(err))
+
+    let updateRatingResponse = await updateRating
+
+    return updateRatingResponse
+}
+
+// Function to delete a rating
+exports.delete = async(ratingID) => {
+
+    let deleteRating = database.deleteResource(database_url, ratings_collection, ratingID)
+                            .then((rating) => rating)
+                            .catch((err) => console.log(err))
+
+    let deleteRatingResponse = await deleteRating
+
+    return deleteRatingResponse
+}
