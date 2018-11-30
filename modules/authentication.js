cf914f09a299e57cce8a033c8b289c56e4f84d5b
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-11-30 19:48:15 +0000

    Working HEAD request for authentication

--- modules/authentication.js
+++ modules/authentication.js
@@ -2,39 +2,35 @@
 
 const bcrypt = require('bcrypt')
 
-const loginsController = require('./logins-controller');
+// Import module for communicating with users backend
+const usersController = require('./users-controller')
 
-exports.loginUser = (request, callback) => {
-	
-	//first check if basic authorization is present
-	if (request.authorization === undefined || request.authorization.basic === undefined){
-		//throw new Error('authorization header missing')
-		let err = {message:'authorization header missing'};
-		console.log("-->" + err.message);
-		callback(err);
-		return;
-	}
-		
-	const auth = request.authorization.basic
-
-	//extract username and password from the auth
-	if (auth.username === undefined || auth.password === undefined){
-		//throw new Error('missing username and/or password')
-		let err = {message:'missing username and/or password'};
-		console.log("-->" + err.message);
-		callback(err);
-		return;
-	}
+exports.checkUserCredentials = async(authorizationHeader) => {
     
-    const user = loginsController.getAll({username:auth.username, passwordHash: auth.password})
-
-    if(user === undefined) {
-        callback(null, {login: false})
-    } else {
-        callback(null, {login: true})
-    }
-}
-
-exports.doPasswordsMatch = () => {
+    // Split word 'Basic' from the Authorization header
+    const [, hash] = authorizationHeader.split(' ')
     
+    // Get the username and password in plain text
+    const userCredentials = Buffer.from(hash, 'base64').toString()
+    
+    // Split the username and password by the colon seperating them
+    const [username, password] = userCredentials.split(':')
+
+    // Retrieve the user from the db that matches the username the user entered
+    const existingUser = await usersController.getAll({username: username})
+
+	// If a user was found with this username, check the password
+	if(existingUser.length > 0) {
+		// Compare the password the user entered with the one stored in db for the user
+		const passwordCorrect = await bcrypt.compare(password, existingUser[0].password)
+
+		if(passwordCorrect) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		// If the username doesn't exist then don't even try to verify user password
+		return false
+	}        
 }
