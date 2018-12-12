4e2267863820e7a5168b45c968f7b46cbd8f569d
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-12-12 09:26:35 +0000

    Added jsdocs to rest of modules

--- modules/images-controller.js
+++ modules/images-controller.js
@@ -5,36 +5,55 @@ const imagesCollection = 'images'
 
 const database = require('./mongodb-database')
 
+/**
+ * Requests and validates a request to add a new image
+ * @param {Object} imageObject - Object representing a new image you want to add
+ * @returns {Object} Result of request if it was successful or not
+ */
 // Function to add a new image
 exports.add = async(imageObject) => {
 
 	const addImage = database.addResourceToCollection(databaseUrl, imagesCollection, imageObject)
 		.then((result) => result)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const addImageResponse = await addImage
 
 	return addImageResponse
 }
 
-// Function to retrieve one image
+/**
+ * Requests and validates a request to retrieve one image by its id
+ * @param {number} imageId - ID of the image you want to retrieve
+ * @returns {Object} Image object retrieved from the request
+ */
 exports.getById = async(imageId) => {
 
 	const getImage = database.getResourceFromCollection(databaseUrl, imagesCollection, imageId)
 		.then((image) => image)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const image = await getImage
 
 	return image
 }
 
-// Function to retrieve all images
-exports.getAll = async() => {
+/**
+ * Requests and validates a request to retrieve all images or all images matching an optional query
+ * @param {Object} [paginationObject] - Optional query object for which images you want to request
+ * @returns {Array} Array of image objects retrieved from the request
+ */
+exports.getAll = async(queryObject) => {
 
-	const results = database.getAllFromCollection(databaseUrl, imagesCollection)
-		.then((results) => results) // Obtains the result from the Promise object
-		.catch((err) => console.log(err)) // If the result was an error then handle the error
+	// Set defaults for search, pagination and sort queries (not yet available to be customised for requesting images)
+	let searchObject = {}
+	let paginationObject = {limit: 0, skip: 0}
+	let sortObject = {}
+
+	// Call database to retrieve all resources from the collection
+	const results = database.getAllFromCollection(databaseURL, imagesCollection, searchObject, paginationObject, sortObject)
+						.then((results) => results) // Obtains the result from the Promise object
+						.catch((reason) => reason) // If the result was an error then handle the error
 
 	// Calls the results function, waits for response before continuing
 	const finalResult = await results
@@ -43,24 +62,33 @@ exports.getAll = async() => {
 	return finalResult
 }
 
-// Function to update a image
+/**
+ * Requests and validates a request to update a image with a new image object
+ * @param {number} imageID - ID of the image you want to update
+ * @param {Object} newImageDetailsObject - Image object you want to replace the existing image with
+ * @returns {Object} Result of request if it was successful or not
+ */
 exports.update = async(imageID, newImageDetailsObject) => {
 
 	const updateImage = database.updateResource(databaseUrl, imagesCollection, imageID, newImageDetailsObject)
 		.then((image) => image)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const updateImageResponse = await updateImage
 
 	return updateImageResponse
 }
 
-// Function to delete a image
+/**
+ * Requests and validates a request to delete a image
+ * @param {number} imageID - ID of the image you want to delete
+ * @returns {Object} Result of request if it was successful or not
+ */
 exports.delete = async(imageID) => {
 
 	const deleteImage = database.deleteResource(databaseUrl, imagesCollection, imageID)
 		.then((image) => image)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const deleteImageResponse = await deleteImage
 
--- modules/notifications-controller.js
+++ modules/notifications-controller.js
@@ -1,40 +1,59 @@
 'use strict'
 
-const databaseURL = 'mongodb://localhost:27017/yummy_recipes'
+const databaseUrl = 'mongodb://localhost:27017/yummy_recipes'
 const notificationsCollection = 'notifications'
 
 const database = require('./mongodb-database')
 
+/**
+ * Requests and validates a request to add a new notification
+ * @param {Object} notificationObject - Object representing a new notification you want to add
+ * @returns {Object} Result of request if it was successful or not
+ */
 // Function to add a new notification
 exports.add = async(notificationObject) => {
 
-	const addNotification = database.addResourceToCollection(databaseURL, notificationsCollection, notificationObject)
+	const addNotification = database.addResourceToCollection(databaseUrl, notificationsCollection, notificationObject)
 		.then((result) => result)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const addNotificationResponse = await addNotification
 
 	return addNotificationResponse
 }
 
-// Function to retrieve one notification
+/**
+ * Requests and validates a request to retrieve one notification by its id
+ * @param {number} notificationId - ID of the notification you want to retrieve
+ * @returns {Object} Notification object retrieved from the request
+ */
 exports.getById = async(notificationId) => {
 
-	const getNotification = database.getResourceFromCollection(databaseURL, notificationsCollection, notificationId)
+	const getNotification = database.getResourceFromCollection(databaseUrl, notificationsCollection, notificationId)
 		.then((notification) => notification)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const notification = await getNotification
 
 	return notification
 }
 
-// Function to retrieve all notifications
-exports.getAll = async() => {
+/**
+ * Requests and validates a request to retrieve all notifications or all notifications matching an optional query
+ * @param {Object} [paginationObject] - Optional query object for which notifications you want to request
+ * @returns {Array} Array of notification objects retrieved from the request
+ */
+exports.getAll = async(queryObject) => {
 
-	const results = database.getAllFromCollection(databaseURL, notificationsCollection)
-		.then((results) => results) // Obtains the result from the Promise object
-		.catch((err) => console.log(err)) // If the result was an error then handle the error
+	// Set defaults for search, pagination and sort queries (not yet available to be customised for requesting notifications)
+	let searchObject = {}
+	let paginationObject = {limit: 0, skip: 0}
+	let sortObject = {}
+
+	// Call database to retrieve all resources from the collection
+	const results = database.getAllFromCollection(databaseURL, notificationsCollection, searchObject, paginationObject, sortObject)
+						.then((results) => results) // Obtains the result from the Promise object
+						.catch((reason) => reason) // If the result was an error then handle the error
 
 	// Calls the results function, waits for response before continuing
 	const finalResult = await results
@@ -43,24 +62,33 @@ exports.getAll = async() => {
 	return finalResult
 }
 
-// Function to update a notification
+/**
+ * Requests and validates a request to update a notification with a new notification object
+ * @param {number} notificationID - ID of the notification you want to update
+ * @param {Object} newNotificationDetailsObject - Notification object you want to replace the existing notification with
+ * @returns {Object} Result of request if it was successful or not
+ */
 exports.update = async(notificationID, newNotificationDetailsObject) => {
 
-	const updateNotification = database.updateResource(databaseURL, notificationsCollection, notificationID, newNotificationDetailsObject)
+	const updateNotification = database.updateResource(databaseUrl, notificationsCollection, notificationID, newNotificationDetailsObject)
 		.then((notification) => notification)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const updateNotificationResponse = await updateNotification
 
 	return updateNotificationResponse
 }
 
-// Function to delete a notification
+/**
+ * Requests and validates a request to delete a notification
+ * @param {number} notificationID - ID of the notification you want to delete
+ * @returns {Object} Result of request if it was successful or not
+ */
 exports.delete = async(notificationID) => {
 
-	const deleteNotification = database.deleteResource(databaseURL, notificationsCollection, notificationID)
+	const deleteNotification = database.deleteResource(databaseUrl, notificationsCollection, notificationID)
 		.then((notification) => notification)
-		.catch((err) => console.log(err))
+		.catch((reason) => reason)
 
 	const deleteNotificationResponse = await deleteNotification
 
