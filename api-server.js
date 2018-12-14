c35800afa062f08b3a6c6456be0469c751a93137
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-12-14 14:14:03 +0000

    Endpoints sending correct status codes

--- api-server.js
+++ api-server.js
@@ -90,7 +90,6 @@ app.get('/api/v1.0/recipes', async(req, res) => {
 
 	// Respond with appropiate status code and body as results array of objects from the query
 	res.status(httpStatus.OK).send(recipes)
-
 })
 
 /**
@@ -103,13 +102,8 @@ app.get('/api/v1.0/recipes/:recipe_id', async(req, res) => {
 	// Call controller to retrieve one recipe using the provided id
 	const recipe = await recipesController.getById(req.params.recipe_id)
 
-	if(recipe) {
-		// Respond with appropiate status code and body as result object of the query
-		res.status(httpStatus.OK).send(recipe)
-	} else {
-		// If length is 0, then it returned an empty object, so resource not found/doesn't exist
-		res.status(httpStatus.NOT_FOUND).send()
-	}
+	// Respond with appropiate status code and body as result object of the query
+	res.status(httpStatus.OK).send(recipe)
 })
 
 /**
@@ -204,15 +198,10 @@ app.delete('/api/v1.0/recipes/:recipe_id', async(req, res) => {
 app.get('/api/v1.0/users', async(req, res) => {
 
 	// Call controller to retrieve all users for the client's query
-	const users = await usersController.getAll(req.query)
+	const users = await usersController.getAll(req.body)
 
-	if(users.length >= 1) {
-		// Respond with appropiate status code and body as results array of objects from the query
-		res.status(httpStatus.OK).send(users)
-	} else {
-		// If length is 0, then it returned an empty object, so resource not found/doesn't exist
-		res.status(httpStatus.NOT_FOUND).send()
-	}
+	// Respond with appropiate status code and body as results array of objects from the query
+	res.status(httpStatus.OK).send(users)
 })
 
 /**
@@ -225,13 +214,8 @@ app.get('/api/v1.0/users/:user_id', async(req, res) => {
 	// Call controller to retrieve one user using the provided id
 	const user = await usersController.getById(req.params.user_id)
 
-	if(user.length >= 1) {
-		// Respond with appropiate status code and body as result object of the query
-		res.status(httpStatus.OK).send(user)
-	} else {
-		// If length is 0, then it returned an empty object, so resource not found/doesn't exist
-		res.status(httpStatus.NOT_FOUND).send()
-	}
+	// Respond with appropiate status code and body as result object of the query
+	res.status(httpStatus.OK).send(user)
 })
 
 /**
@@ -302,6 +286,7 @@ app.get('/api/v1.0/ratings', async(req, res) => {
 	// Waits for response from controller before continuing (async/await)
 	const ratings = await ratingsController.getAll()
 
+	// Respond with appropiate status code and body as results array of objects from the query
 	res.status(httpStatus.OK).send(ratings)
 })
 
@@ -315,6 +300,7 @@ app.get('/api/v1.0/ratings/:rating_id', async(req, res) => {
 	// Call controller to retrieve one rating
 	const rating = await ratingsController.getById(req.params.rating_id)
 
+	// Respond with appropiate status code and body as result object of the query
 	res.status(httpStatus.OK).send(rating)
 })
 
@@ -329,9 +315,11 @@ app.post('/api/v1.0/ratings', async(req, res) => {
 	const response = await ratingsController.add(req.body)
 
 	if(response) {
-		res.status(httpStatus.OK).send('Rating added succesfully\n')
+		// If adding rating was successful, return 201 status code and object confirming request response
+		res.status(httpStatus.CREATED).send({status: 'success', ratingAddedSuccessfully: response})
 	} else {
-		res.status(httpStatus.BAD_REQUEST).send('There was an error posting your rating\n')
+		// If adding rating was unsuccessful, return 400 status code and object confirming request response
+		res.status(httpStatus.BAD_REQUEST).send({status: 'fail', ratingAddedSuccessfully: response})
 	}
 })
 
